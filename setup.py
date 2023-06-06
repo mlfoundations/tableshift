ce20e03ae765bd2280bd2bd63cae65d127109399
jpgard <joshua.patrick.gardner@gmail.com>
2023-06-06 14:41:29 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tableshift',
+      version='0.1',
+      description='A tabular data benchmarking toolkit.',
+      author='Josh Gardner',
+      author_email='jpgard@cs.washington.edu',
+      packages=['tableshift'],
+      data_files=[('tableshift/datasets',
+                   ['tableshift/datasets/nhanes_data_sources.json',
+                    'tableshift/datasets/icd9-codes.json'])]
+      )
