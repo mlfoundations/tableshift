c980dbbf98473ae5676ffa5696880ff3376821ad
jpgard <joshua.patrick.gardner@gmail.com>
2023-06-12 22:36:01 -0400

    add url

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 setup(name='tableshift',
       version='0.1',
+      url='https://tableshift.org',
       description='A tabular data benchmarking toolkit.',
       author='Josh Gardner',
       author_email='jpgard@cs.washington.edu',
